73c422fb626fae1c03e02210dfaac1f5714344e1
natalie <natalie.itzhak@gmail.com>
2021-03-15 16:03:14 +0200

    Cardclass

--- src/App.js
+++ src/App.js
@@ -9,25 +9,24 @@ import Card from './components/Cards/Cards';
 import Qst1 from './components/Quiz/Qst1';
 import Qst2 from './components/Quiz/qst2';
 import Quiz from './components/Quiz/quiz_3.3';
+import React from 'react';
 
 
 
 
-function App() {
-  return (
-    <div>
+// function App() {
+//   return (
+//     <div>
  {/* <Basics /> */}
  {/* <Box1 /> */}
 {/* <Quiz /> */}
 {/* <Buttons/> */}
-<Card 
+{/* <Card 
 src={'https://picsum.photos/200'}
 textTitle={'title 1'}
 desc={'description 1'}
 link1={'Explore'}
 link2={'Share'}
-
-
 
 />
 <Card 
@@ -45,10 +44,25 @@ desc={'description 3'}
 link1={'Explore'}
 link2={'Share'}
 
-/>
-
+/> */}
+{/* 
     </div>
   );
+}
+
+export default App; */}
+
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Card src={'https://picsum.photos/200'} textTitle={'title 1'} desc={'description 1'} link1={'Explore'} link2={'Share'}/>
+        <Card src={'https://picsum.photos/200'} textTitle={'title 2'} desc={'description 2'} link1={'Explore'} link2={'Share'}/>
+        <Card src={'https://picsum.photos/200'} textTitle={'title 3'} desc={'description 3'} link1={'Explore'} link2={'Share'}/>
+      </div>
+    );
+  };
 }
 
 export default App;
