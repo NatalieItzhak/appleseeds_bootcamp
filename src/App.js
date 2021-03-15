eacd00c048cfb341b281119e7d0c06ea56521028
natalie <natalie.itzhak@gmail.com>
2021-03-15 14:00:34 +0200

    Buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import react, {Component} from 'react';
-import './boxes.css'
+// import './boxes.css'
+import './Buttons.css'
 import Basics from './components/Basics/Basic2.2'
 import Box1 from './components/Boxes/Box1';
+import Buttons from './components/Buttons/Buttons';
 import Qst1 from './components/Quiz/Qst1';
 import Qst2 from './components/Quiz/qst2';
 import Quiz from './components/Quiz/quiz_3.3';
@@ -14,7 +16,10 @@ function App() {
     <div>
  {/* <Basics /> */}
  {/* <Box1 /> */}
-<Quiz />
+{/* <Quiz /> */}
+<Buttons/>
+
+
     </div>
   );
 }
---
+++ src/components/Buttons/Buttons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './Buttons.css';
+
+const Button = () => {
+    return (
+        <div>
+            <button className="bold" type='submit'>Important</button>
+            <button  type='submit'>Not Important</button>
+        </div>
+    );
+};
+
+export default Button;
